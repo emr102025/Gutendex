a62ba687412b52f81f136a73a65ffb1933affec8
emr102025 <emr102025@gmail.com>
2026-02-19 11:37:08 +0100

    Implemented a input field and a button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import CategoryMenu from "../CategoryMenu/CategoryMenu";
 
 export default function Header() {
-  const [search, setSearch] = useState("");
-
   return (
     <nav style={{ background: "black" }}>
       <Link to="/">
         <h1>Gutendex</h1>
       </Link>
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search for book.."
-      />
-      <button type="submit">Search</button>
       <button>Favorites</button>
       <CategoryMenu />
     </nav>
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import styles from "./SearchBar/SearchBar.module.css";
+
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
+  return (
+    <form>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search for book.."
+        className={styles.searchBar}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
