99e6613d88872cf500cd4c3995288f4bb3f90065
emr102025 <emr102025@gmail.com>
2026-02-21 18:05:43 +0100

    More canges to Header css

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,12 +1,13 @@
 import { useTheme } from "../Context/ThemeContext.jsx";
 import styles from "../Header/Header.module.css";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 export default function ThemeSwitcher() {
   const { theme, toggleTheme } = useTheme();
   return (
     <>
       <button className={styles.theme} onClick={toggleTheme}>
-        Mode: {theme}
+        {theme === "light" ? <FiSun /> : <FiMoon />}
       </button>
     </>
   );
