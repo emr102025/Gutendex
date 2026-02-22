550ba55968b3025564716f2320a4d0c6d2fbeb84
emr102025 <emr102025@gmail.com>
2026-02-22 19:01:05 +0100

    Made the structure for BookDetails on DetailsPage

--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <CategoryPage />,
       },
       {
-        path: "book/:bookId",
+        path: "book/:id",
         element: <DetailsPage />,
       },
       {
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,34 +1,55 @@
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import BookCard from "../../components/BookCard/BookCard.jsx";
-import BookList from "../../components/BookList/BookList.jsx";
+import { useParams } from "react-router-dom";
 import Pagination from "../../components/Pagination/Pagination.jsx";
 import { fetchBookDetails } from "../../api/axiosGutendex.js";
 
 export default function DetailsPage() {
-  const [books, setBooks] = useState([]);
-  const location = useLocation();
+  const [book, setBook] = useState(null);
+  const { id } = useParams(); // This is to get the book ID from the URL
 
-  // get query from url
-  const searchResult = new URLSearchParams(location.search).get("q");
   useEffect(() => {
-    if (!searchResult) return;
-
-    const getBooks = async () => {
+    const getBook = async () => {
       try {
-        const data = await fetchBookDetails(searchResult);
-        setBooks(data.results); // Gutendex puts books inside "results"
+        const data = await fetchBookDetails(id);
+        setBook(data); // Gutendex puts books inside "results"
       } catch (error) {
         console.error(error);
       }
     };
 
-    getBooks();
-  }, [searchResult]);
+    getBook();
+  }, [id]);
+
+  if (!book) return <p>Loading...</p>;
 
   return (
-    <>
-      <h1>DetailsPage</h1>
-    </>
+    <article>
+      <h1>{book.title}</h1>
+      <img src={book.formats?.["image/jpeg"]} alt={book.title} />
+      <p>
+        <strong>Author:</strong> {book.authors?.[0]?.name}
+      </p>
+      <p>
+        <strong>Downloads:</strong> {book.download_count}
+      </p>
+      <p>
+        <strong>Category:</strong> {book.subjects?.join(", ")}
+      </p>
+      <p>
+        <strong>Language:</strong> {book.languages?.join(", ")}
+      </p>
+      <p>
+        <strong>Read online:</strong>{" "}
+        <a
+          href={book.formats?.["text/html"]}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Book
+        </a>
+      </p>
+
+      <button>Add to Favorites</button>
+    </article>
   );
 }
