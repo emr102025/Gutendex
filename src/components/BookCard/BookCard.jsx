07eff7ede96bf0e0aa7873552d5b4bcd7332ac6e
emr102025 <emr102025@gmail.com>
2026-02-22 18:11:58 +0100

    Added styles to bookCards and changes some styling on other components

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -3,18 +3,20 @@ import styles from "./BookCard.module.css";
 
 export default function BookCard({ book }) {
   return (
-    <Link to={`/book/${book.id}`} className={styles.bookCard}>
-      <img
-        src={book.formats["image/jpeg"] || "Missing Cover"}
-        alt={book.title}
-        className={styles.bookCover}
-      />
-      <section className={styles.info}>
-        <h3 className={styles.bookTitle}>{book.title || "Unknown Title"}</h3>
-        <p className={styles.author}>
-          {book.authors?.map((a) => a.name).join(", ") || "Unknown Author"}
-        </p>
-      </section>
-    </Link>
+    <article className={styles.bookCard}>
+      <Link to={`/book/${book.id}`}>
+        <img
+          src={book.formats["image/jpeg"] || "Missing Cover"}
+          alt={book.title}
+          className={styles.bookCover}
+        />
+        <section className={styles.info}>
+          <h3 className={styles.bookTitle}>{book.title || "Unknown Title"}</h3>
+          <p className={styles.author}>
+            {book.authors?.map((a) => a.name).join(", ") || "Unknown Author"}
+          </p>
+        </section>
+      </Link>
+    </article>
   );
 }
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import BookCard from "../../components/BookCard/BookCard.jsx";
+import BookList from "../../components/BookList/BookList.jsx";
+import Pagination from "../../components/Pagination/Pagination.jsx";
+import { fetchBookDetails } from "../../api/axiosGutendex.js";
+
 export default function DetailsPage() {
+  const [books, setBooks] = useState([]);
+  const location = useLocation();
+
+  // get query from url
+  const searchResult = new URLSearchParams(location.search).get("q");
+  useEffect(() => {
+    if (!searchResult) return;
+
+    const getBooks = async () => {
+      try {
+        const data = await fetchBookDetails(searchResult);
+        setBooks(data.results); // Gutendex puts books inside "results"
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getBooks();
+  }, [searchResult]);
+
   return (
     <>
       <h1>DetailsPage</h1>
